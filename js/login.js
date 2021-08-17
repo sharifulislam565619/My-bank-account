cbe3d48229fd1e21c365e78e444b14ec9d66d840
sharifulislam565619 <sharifulislam565619@gmail.com>
2021-08-17 17:02:38 +0600

    some change

--- js/login.js
+++ js/login.js
@@ -1,9 +1,10 @@
 document.getElementById("submit-button").addEventListener("click", function () {
     const userEmail = document.getElementById("user-email").value
     const userPassword = document.getElementById("user-password").value
-    if (userEmail == "shihab.ss46@gmail.com" && userPassword == 12345678) {
-        window.location.href = 'inside-bank.html'
-    } else {
-        document.getElementById("error-message").innerText="Email or password incorrect"
-}
+//     if (userEmail == "shihab.ss46@gmail.com" && userPassword == 12345678) {
+//         window.location.href = 'inside-bank.html'
+//     } else {
+//         document.getElementById("error-message").innerText="Email or password incorrect"
+// }
+     window.location.href = 'inside-bank.html'
 })
